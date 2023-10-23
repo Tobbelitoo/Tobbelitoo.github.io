7f07b3aa4688196095ed33ef7394074485e5fbb9
Tobbelitoo <52721729+Tobbelitoo@users.noreply.github.com>
2023-10-23 11:19:02 +0200

    Added more questions

--- quizApp/script/javascript.js
+++ quizApp/script/javascript.js
@@ -22,8 +22,16 @@ let topics = [
 		questionsArray: 'htmlhead'
 	},
 	{
+		name: 'HTML - Body',
+		questionsArray: 'htmlbody'
+	},
+	{
 		name: 'CSS - Flexbox parent',
 		questionsArray: 'cssflexboxparent'
+	},
+	{
+		name: 'CSS - Flexbox child',
+		questionsArray: 'cssflexboxchild'
 	},
 	{
 		name: 'GITHUB - Uppdatera repo',
@@ -57,10 +65,15 @@ function prepareGame(e) {
 		case "HTML - Head":
 			questions = htmlhead;
 			break;
+		case "HTML - Body":
+			questions = htmlbody;
+			break;
 		case "CSS - Flexbox parent":
 			questions = cssflexboxparent;
 			break;
-	}
+		case "CSS - Flexbox child":
+			questions = cssflexboxchild;
+			break;	}
 	totalQuestions = questions.length;
 	console.log(totalQuestions);
 }
@@ -179,103 +192,124 @@ var matematik = [
 
 
 var htmlhead = [
-	{
-		question: 'Vilka element används i head?',
-		answers: [
-			{ text: 'title, meta, link, script', correct: true }
-		]
-	},
-	{
-		question: 'Vilka meta-element finns?',
-		answers: [
-			{ text: 'meta charset, meta viewport', correct: true }
-		]
-	},
-	{
-		question: 'Beskriv meta charset',
-		answers: [
-			{ text: 'meta charset="UTF-8"', correct: true }
-		]
-	},
-	{
-		question: 'Beskriv meta viewport',
-		answers: [
-			{ text: 'meta name="viewport" content="width=device-width, initial-scale=1.0"', correct: true }
-		]
-	},
-	{
-		question: 'Beskriv link',
-		answers: [
-			{ text: 'link rel="stylesheet" href="css/style.css"', correct: true }
-		]
-	},
-	{
-		question: 'Besrkvit script',
-		answers: [
-			{ text: 'script defer src="script/javascript.js"></script', correct: true }
-		]
-	}
+{question: 'Vilka element används i head?',answers: [{ text: 'title, meta, link, script', correct: true }]},
+{question: 'Vilka meta-element finns?',answers: [{ text: 'meta charset, meta viewport', correct: true }]},
+{question: 'Beskriv meta charset',answers: [{ text: 'meta charset="UTF-8"', correct: true }]},
+{question: 'Beskriv meta viewport',answers: [{ text: 'meta name="viewport" content="width=device-width, initial-scale=1.0"', correct: true }]},
+{question: 'Beskriv link',answers: [{ text: 'link rel="stylesheet" href="css/style.css"', correct: true }]},
+{question: 'Besrkvit script',answers: [{ text: 'script defer src="script/javascript.js"></script', correct: true }]}
 ];
 
+var htmlbody = [
+{question: 'Räkna upp body element, de med beskrivande namn',answers: [{ text: 'container, content, aside, main, header, footer', correct: true }]},
+{question: 'Hur lägger man in flera klasser eller id:n i ett element',answers: [{ text: 'class="klass1, klass2"', correct: true }]},
+{question: 'I vilket format skriver man klasser och id:n',answers: [{ text: '"btn-grid", "start-btn"', correct: true }]},
+{question: 'Hur skriver man en selekt-meny',answers: [{ text: 'select name="cars"   option value="volvo"  Volvo /option  /select', correct: true }]},
+{question: 'Hur skriver man en rubrik till select-menyn',answers: [{ text: 'label for="cars"  Choose a car:  /label', correct: true }]}
+];
 
 
 
 var cssflexboxparent = [
-	{
-		question: 'Räkna upp flexbox properties',
-		answers: [
-			{ text: 'display, flex-direction, justify-content, gap, flex-wrap, flex-direction, align-items', correct: true }
-		]
-	},
-	{
-		question: 'Beskriv display values',
-		answers: [
-			{ text: 'flex', correct: true }
-		]
-	},
-	{
-		question: 'Beskriv flex-direction values',
-		answers: [
-			{ text: 'column, row (row default)', correct: true }
-		]
-	},
-	{
-		question: 'Beskriv justify-content values',
-		answers: [
-			{ text: 'space-between', correct: true }
-		]
-	},
-	{
-		question: 'Beskriv gap',
-		answers: [
-			{ text: '1rem, mellanrum mellan flex-items', correct: true }
-		]
-	},
-	{
-		question: 'Beskriv flex-wrap',
-		answers: [
-			{ text: 'wrap (default nowrap)', correct: true }
-		]
-	},
-	{
-		question: 'Beskriv flex-direction',
-		answers: [
-			{ text: 'row, column (row default)', correct: true }
-		]
-	},
-	{
-		question: 'Beskriv justify-content',
-		answers: [
-			{ text: 'center, flex-start, flex-end, space-between, space-around, space-evenly (om space finns)', correct: true }
-		]
-	},
-	{
-		question: 'Beskriv align-items',
-		answers: [
-			{ text: 'flex-start, flex-end, center, stretch (default) (om space finns)', correct: true }
-		]
-	}
+{question: 'Räkna upp flexbox properties',answers: [{ text: 'display, flex-direction, justify-content, gap, flex-wrap, flex-direction, align-items', correct: true }]},
+{question: 'Beskriv display values',answers: [{ text: 'flex', correct: true }]},
+{question: 'Beskriv flex-direction values',answers: [{ text: 'column, row (row default)', correct: true }]},
+{question: 'Beskriv justify-content values',answers: [{ text: 'center, flex-start, flex-end, space-between, space-around, space-evenly (om space finns)', correct: true }]},
+{question: 'Beskriv gap',answers: [{ text: '1rem, mellanrum mellan flex-items', correct: true }]},
+{question: 'Beskriv flex-wrap',answers: [{ text: 'wrap (default nowrap)', correct: true }]},
+{question: 'Beskriv align-items',answers: [{ text: 'flex-start, flex-end, center, stretch (default) (om space finns)', correct: true }]}
 ];
+
+var cssflexboxchild = [
+{question: 'Räkna upp flexbox properties för flex item',answers: [{ text: 'flex-grow, flex-shrink, flex-basis', correct: true }]},
+{question: 'Beskriv flex-grow',answers: [{ text: '1 , ett nummer som beskriver hur mycket olika element ska växa i förhållande till varandra', correct: true }]},
+{question: 'Beskriv flex-shrink',answers: [{ text: '1 , hur ett element ska förminskas i förhållande till de andra i samma container', correct: true }]},
+{question: 'Beskriv flex-basis',answers: [{ text: '100px , sätter den initiala längden av ett item', correct: true }]},
+{question: 'Vad är en shorthand för flex items properties',answers: [{ text: 'flex , shorthand för flex-grow, flex-shrink, flex-basis. Sätter den flexibla längden på ett item', correct: true }]},
+{question: 'Vilka värden kan flex ha',answers: [{ text: '0, 1, 50%, 100%, m.fl. ?', correct: true }]},
+{question: 'Vad innebär flex: 1',answers: [{ text: 'flex-grow: 1, flex-shrink: 1, flex-basis: 0', correct: true }]}
+];
+
+
+
+
+
+/*  
+
+
+ONE ANSWER...
+
+
+var questions.. = [
+{question: '',answers: [{ text: '', correct: true }]},
+{question: '',answers: [{ text: '', correct: true }]},
+{question: '',answers: [{ text: '', correct: true }]},
+{question: '',answers: [{ text: '', correct: true }]},
+{question: '',answers: [{ text: '', correct: true }]},
+{question: '',answers: [{ text: '', correct: true }]}
+];
+
+
+var questions.. = [
+{question: '',answers: [{ text: '', correct: true }]},
+{question: '',answers: [{ text: '', correct: true }]},
+{question: '',answers: [{ text: '', correct: true }]},
+{question: '',answers: [{ text: '', correct: true }]},
+{question: '',answers: [{ text: '', correct: true }]},
+{question: '',answers: [{ text: '', correct: true }]}
+];
+
+
+var questions.. = [
+{question: '',answers: [{ text: '', correct: true }]},
+{question: '',answers: [{ text: '', correct: true }]},
+{question: '',answers: [{ text: '', correct: true }]},
+{question: '',answers: [{ text: '', correct: true }]},
+{question: '',answers: [{ text: '', correct: true }]},
+{question: '',answers: [{ text: '', correct: true }]}
+];
+
+
+
+
+
+
+MANY ANSWERS
+
+
+
+var questions.. = [
+{question: '',answers: [{ text: '', correct: true }, { text: '', correct: false }, { text: '', correct: false }, { text: '', correct: false }]},
+{question: '',answers: [{ text: '', correct: true }, { text: '', correct: false }, { text: '', correct: false }, { text: '', correct: false }]},
+{question: '',answers: [{ text: '', correct: true }, { text: '', correct: false }, { text: '', correct: false }, { text: '', correct: false }]},
+{question: '',answers: [{ text: '', correct: true }, { text: '', correct: false }, { text: '', correct: false }, { text: '', correct: false }]},
+{question: '',answers: [{ text: '', correct: true }, { text: '', correct: false }, { text: '', correct: false }, { text: '', correct: false }]},
+{question: '',answers: [{ text: '', correct: true }, { text: '', correct: false }, { text: '', correct: false }, { text: '', correct: false }]}
+];
+
+
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
